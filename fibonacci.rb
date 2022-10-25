95237e68f1119cdb06743ee10af63cbd98c8bebc
Jeremy <jeremy.steven.bess@gmail.com>
2022-10-25 16:30:31 -0700

    Create fibs_rec method

--- fibonacci.rb
+++ fibonacci.rb
@@ -14,4 +14,18 @@ def fibs(n)
 end
 
 fibs 8
-            
+
+def fibs_rec(n)
+    if n == 1
+        return [0, 1]
+    elsif n == 0
+        return [0]
+    else
+        arr = fibs_rec(n-1)
+        p arr << arr[-1] + arr[-2]
+    end
+end
+
+
+
+ fibs_rec(8)
